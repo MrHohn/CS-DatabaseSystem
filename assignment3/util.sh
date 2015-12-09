88bd64c9665257007cf60a4990cfacfc4a94c86f
MrHohn <zzhonzi@gmail.com>
2015-12-09 14:48:38 -0500

    Finished set up for script and makefile

---
+++ assignment3/util.sh
@@ -0,0 +1,14 @@
+case $1 in
+start)
+	echo "--- now start the hadoop ---"
+	start-dfs.sh
+	start-yarn.sh
+	echo "--- done ---"
+	;;
+stop)
+    echo "--- now stop the hadoop ---"
+    stop-dfs.sh
+	stop-yarn.sh
+	echo "--- done ---"
+	;;
+esac
